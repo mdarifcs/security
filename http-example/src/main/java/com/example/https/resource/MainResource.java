3b98cceb598df336289946a4fde0ef5bde664b0b
mohammad arif <mdarif.cs@gmail.com>
2019-09-07 18:47:36 +0530

    HTTPS Protocol Example

---
+++ http-example/src/main/java/com/example/https/resource/MainResource.java
@@ -0,0 +1,14 @@
+package com.example.https.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainResource {
+
+	@GetMapping("/getData")
+	public String getMessage() {
+		return "Accessed By HTTPS Protocol";
+	}
+
+}
